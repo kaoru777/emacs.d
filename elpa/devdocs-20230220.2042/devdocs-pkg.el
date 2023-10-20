94589a3ecd7e22119962d0b0b4958cf3c9d9ec1f
kaoru <kaoru@steinboeck.dev>
2023-10-20 09:27:00 +0200

    added elpa

---
+++ elpa/devdocs-20230220.2042/devdocs-pkg.el
@@ -0,0 +1,14 @@
+(define-package "devdocs" "20230220.2042" "Emacs viewer for DevDocs"
+  '((emacs "27.1"))
+  :commit "2988d4d201df16d72c3bea465d2b93b554dbddfc" :authors
+  '(("Augusto Stoffel" . "arstoffel@gmail.com"))
+  :maintainers
+  '(("Augusto Stoffel" . "arstoffel@gmail.com"))
+  :maintainer
+  '("Augusto Stoffel" . "arstoffel@gmail.com")
+  :keywords
+  '("help")
+  :url "https://github.com/astoff/devdocs.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/expand-region-20231016.51/expand-region-pkg.el
@@ -0,0 +1,14 @@
+(define-package "expand-region" "20231016.51" "Increase selected region by semantic units."
+  '((emacs "24.4"))
+  :commit "d91935345581a034c0424cf2885f6a25d329f888" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainers
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("marking" "region")
+  :url "https://github.com/magnars/expand-region.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
